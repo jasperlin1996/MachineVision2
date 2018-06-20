35cc187244b854560d8958151ee3d89ab9f3d4a4
jasperlin1996 <jasperlin1996@gmail.com>
2018-06-21 00:03:15 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ bgSubThreshold = 100
 bgModel = cv2.createBackgroundSubtractorMOG2(0, bgSubThreshold)
 imgSkin = np.zeros((int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)),int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)),3), np.uint8)
 
-output = np.zeros((340,680*2,3), dtype = np.uint8)
-
+output = np.zeros((480,640*2,3), dtype = np.uint8)
+out = cv2.VideoWriter('./hw2.avi', cv2.VideoWriter_fourcc(*'XVID'), 20.0, (1280, 480))
 model = load_model('test_model.h5')
 
 result_char = ['子','丑','寅','卯','辰','巳','午','未','申','酉','戌','亥']
@@ -24,6 +24,8 @@ def inner_product(p1, p2, p3):
 	return (np.dot(v1,v2)/(np.sqrt(v1[0]**2+v1[1]**2)*np.sqrt(v2[0]**2+v2[1]**2)))
 
 j = 0
+current_text = ' '
+recording = 0
 while(True):
 	j += 1
 	ret, frame = cap.read()
@@ -85,7 +87,7 @@ while(True):
 
 	im3 = cv2.resize(im2,(0,0),fx = 0.1, fy = 0.1)
 
-	im3 = np.reshape(im3, im2.shape[0]*im2.shape[1])
+	im3 = np.reshape(im3, im3.shape[0]*im3.shape[1])
 
 	if j % 30 == 0:
 		ans = model.predict(np.array([im3]))
@@ -100,19 +102,36 @@ while(True):
 				if value == max:
 					if value > 0.5:
 						print("we got : {}".format(result_char[index]))
+						current_text = result_char[index]
 					else:
 						print("we got nothing")
-						cv2.puttext(output,result_char[index],(10,10),cv2.FONT_HERSHEY_SIMPLEX,0.75,(0, 255, 255),1,cv2.LINE_AA)
+						current_text = 'N'
 
-	
 
-	cv2.rectangle(frame, (150, 70), (640-150, 480-70), (255, 0, 0), 5)
+	if current_text != 'N' and current_text != ' ':
+		for i, value in enumerate(result_char):
+			if value == current_text:
+				s = 'D:\\Downloads\\MachineVision2-master\\Picture\\' + str(i + 1) + '.jpg'
+				break
+		temp = cv2.imread(s)
+		temp = cv2.resize(temp, (60, 60))
+		output[10: 70, 10: 70, :] = temp
+
+	else:
+		cv2.putText(output,current_text,(10,50),cv2.FONT_HERSHEY_SIMPLEX,0.75,(0, 255, 255),1,cv2.LINE_AA)
+	cv2.rectangle(output, (150+640, 70), (640-150+640, 480-70), (255, 0, 0), 5)
+	cv2.rectangle(output, (150, 70), (640-150, 480-70), (255, 0, 0), 5)
 	frame = frame[:,::-1,:]
 	cv2.imshow('webcam', frame)
 	cv2.imshow('output', output)
+	if cv2.waitKey(1) & 0xFF == ord('r'):
+		recording = 1
+	if recording == 1:
+		out.write(output)
+		print("recording----------------")
 	if cv2.waitKey(25) & 0xFF == ord('q'):
 		break
 
 cap.release()
-
+out.release()
 cv2.destroyAllWindows()
